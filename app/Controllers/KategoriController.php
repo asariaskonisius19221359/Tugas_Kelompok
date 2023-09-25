3f9f6f507b4fef130bf80821a90aadcf3d2e4ef0
Granndr <idofuture@gmail.com>
2023-09-25 16:17:15 +0700

    Nambahin Controller Kategori

---
+++ app/Controllers/KategoriController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class KategoriController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+
+    public function kategori_pribadi(){
+
+    }
+
+    public function kategori_usaha(){
+
+    }
+
+    public function kategori_informasi(){
+
+    }
+
+    public function kategori_hiburan(){
+        
+    }
+}
