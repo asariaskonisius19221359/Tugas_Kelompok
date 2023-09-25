b539c0508f14a413ff49f9aed27232dbfead3e32
susmthaaa <yustinayustina886@gmail.com>
2023-09-25 14:05:12 +0700

    Nambahin Controller Beranda

---
+++ app/Controllers/BerandaController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class BerandaController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+
+    public function nama_pengguna(){
+
+    }
+
+    public function informasi_profil(){
+
+    }
+
+    public function status_atau_bio(){
+
+    }
+
+    public function pengetahuan_privasi(){
+        
+    }
+}
