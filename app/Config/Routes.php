8f2106eb56da586846eb0c1927b8429b6fb19a83
Granndr <idofuture@gmail.com>
2023-10-08 16:38:53 +0700

    Database Awal

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->get('/anggota', 'AnggotaController::tampil');
+$routes->get('/anggota/form', 'AnggotaController::form');
+$routes->post('/anggota', 'AnggotaController::tambah');
